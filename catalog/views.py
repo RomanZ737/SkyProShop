13787d38c75ae1dc0df91727a2edcf6d188f0250
Roman <pomanz@mail.ru>
2026-05-24 22:55:12 +0300

    catalog updated

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, Contacts
 
 
 def home(request):
+    for product in Product.objects.all().order_by('-created_at')[:5]:
+        print(product.name)
     return render(request, 'catalog/home.html')
 
 
@@ -13,6 +16,11 @@ def contacts(request):
         message = request.POST['message']
         return HttpResponse(f'Уважаемый, {name}, Ваше сообщение получено\n'
                             f'Мы свяжемся с вами по телефону: {phone}')
-    return render(request, 'catalog/contacts.html')
+    contacts = Contacts.objects.first()
+    print(contacts.country, contacts.tax_num, contacts.address)
+    return render(request, 'catalog/contacts.html',
+                  context={'country': contacts.country,
+                           'tax_num': contacts.tax_num,
+                           'address': contacts.address,})
 
 
